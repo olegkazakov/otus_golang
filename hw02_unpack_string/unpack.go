214ff065e9300b2efbd6dba6d4dd256b657ffeef
Oleg Kazakov <kazakov@atlas.ru>
2023-02-07 14:19:07 +0300

    HW02: Unpack implementation

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	if isNotValidString(s) {
+		return "", ErrInvalidString
+	}
+
+	var builder strings.Builder
+
+	for i, r := range s {
+		char := string(r)
+
+		if unicode.IsDigit(r) {
+			continue
+		}
+
+		if (i < len(s)-1) && (unicode.IsDigit(rune(s[i+1]))) {
+			n, err := strconv.Atoi(string(s[i+1]))
+			if err != nil {
+				return "", err
+			}
+
+			builder.WriteString(strings.Repeat(char, n))
+			continue
+		}
+
+		builder.WriteString(char)
+	}
+
+	return builder.String(), nil
+}
+
+func isNotValidString(s string) bool {
+	return regexp.MustCompile(`^\d|\d\d| `).MatchString(s)
 }
