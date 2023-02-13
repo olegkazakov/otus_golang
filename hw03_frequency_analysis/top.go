04bc8033191d8cf6cf98b7a52388023ece9c255c
Oleg Kazakov <kazakov@atlas.ru>
2023-02-13 21:36:12 +0300

    HW03: Base implementation

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	if s == "" {
+		return []string{}
+	}
+
+	return countAndTrim(strings.Fields(s), 10)
+}
+
+func countAndTrim(fields []string, length int) []string {
+	counter := make(map[string]int)
+	for _, field := range fields {
+		counter[field]++
+	}
+
+	words := make([]string, 0, length)
+	for word := range counter {
+		words = append(words, word)
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		if counter[words[i]] == counter[words[j]] {
+			return words[i] < words[j]
+		}
+		return counter[words[i]] > counter[words[j]]
+	})
+
+	if len(words) < length {
+		return words
+	}
+
+	return words[:length]
 }
