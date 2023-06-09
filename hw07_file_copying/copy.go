3b24294d7c653a2a31a44d96d56ac4bd72beac01
Oleg Kazakov <olegkazakov31@gmail.com>
2023-06-09 19:51:45 +0300

    Hw07 file copying (#7)
    * HW07: Fixed module
    
    * HW07: Added cheggaaa/pb library
    
    * HW07: Implemented Copy function
    
    * HW07: Added tests
    
    * HW07: Removed sync file
    
    * HW07: Cleared copy.go
    
    * HW07: Fixed err hiding in copy.go
    
    Co-authored-by: ityulkanov <ityulkanov@gmail.com>
    
    * HW07: Fixed hidden variables in copy_test.go
    
    ---------
    
    Co-authored-by: Oleg Kazakov <kazakov@atlas.ru>
    Co-authored-by: ityulkanov <ityulkanov@gmail.com>

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,103 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedFile                       = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize                 = errors.New("offset exceeds file size")
+	ErrSourceFileIsNotSpecified              = errors.New("source file is not specified")
+	ErrDestinationFileIsNotSpecified         = errors.New("destination file is not specified")
+	ErrSourceAndDestinationPathsAreIdentical = errors.New("source and destination paths are identical")
+	ErrNegativeOffset                        = errors.New("negative offset")
+	ErrNegativeLimit                         = errors.New("negative limit")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" {
+		return ErrSourceFileIsNotSpecified
+	}
+
+	if toPath == "" {
+		return ErrDestinationFileIsNotSpecified
+	}
+
+	if fromPath == toPath {
+		return ErrSourceAndDestinationPathsAreIdentical
+	}
+
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	defer func() error {
+		err = sourceFile.Close()
+		if err != nil {
+			return err
+		}
+
+		err = destFile.Close()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}()
+
+	sourceFileStat, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	sourceFileSize := sourceFileStat.Size()
+	if offset > sourceFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	readLimit := limit
+	if readLimit == 0 {
+		readLimit = sourceFileSize
+	}
+
+	if readLimit > sourceFileSize-offset {
+		readLimit = sourceFileSize - offset
+	}
+
+	_, err = sourceFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(readLimit)
+	barReader := bar.NewProxyReader(sourceFile)
+	defer bar.Finish()
+
+	_, err = io.CopyN(destFile, barReader, readLimit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,70 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	var (
+		source      = "testdata/input.txt"
+		destination = "testdata/output.txt"
+		testOffset  int64
+		testLimit   int64
+	)
+
+	t.Run("empty source filename", func(t *testing.T) {
+		err := Copy("", destination, testOffset, testLimit)
+		require.ErrorIs(t, err, ErrSourceFileIsNotSpecified)
+	})
+
+	t.Run("empty destination filename", func(t *testing.T) {
+		err := Copy(source, "", testOffset, testLimit)
+		require.ErrorIs(t, err, ErrDestinationFileIsNotSpecified)
+	})
+
+	t.Run("source and destination file paths are identical", func(t *testing.T) {
+		err := Copy(source, source, testOffset, testLimit)
+		require.ErrorIs(t, err, ErrSourceAndDestinationPathsAreIdentical)
+	})
+
+	t.Run("unsupported source file", func(t *testing.T) {
+		err := Copy("/dev/urandom", destination, testOffset, testLimit)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("negative testOffset", func(t *testing.T) {
+		err := Copy(source, destination, -1, testLimit)
+		require.ErrorIs(t, err, ErrNegativeOffset)
+	})
+
+	t.Run("negative testLimit", func(t *testing.T) {
+		err := Copy(source, destination, testOffset, -1)
+		require.ErrorIs(t, err, ErrNegativeLimit)
+	})
+
+	t.Run("testOffset exceeds file size", func(t *testing.T) {
+		sourceFileStat, _ := os.Stat(source)
+		err := Copy(source, destination, sourceFileStat.Size()+1, testLimit)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("testLimit greater than source file size", func(t *testing.T) {
+		sourceFileStat, _ := os.Stat(source)
+		sourceFileSize := sourceFileStat.Size()
+		err := Copy(source, destination, testOffset, sourceFileSize+1024)
+		require.Nil(t, err)
+		destinationFileStat, _ := os.Stat(destination)
+		require.Equal(t, sourceFileSize, destinationFileStat.Size())
+	})
+
+	t.Run("successful case", func(t *testing.T) {
+		sourceFileStat, _ := os.Stat(source)
+		err := Copy(source, destination, 0, 0)
+		require.Nil(t, err)
+		destinationFileStat, _ := os.Stat(destination)
+		require.Equal(t, sourceFileStat.Size(), destinationFileStat.Size())
+	})
 }
