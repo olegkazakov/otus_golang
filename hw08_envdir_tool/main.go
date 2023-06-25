2dd4ebee9aefa342fb4674e9ed3db427e693c1f1
Oleg Kazakov <kazakov@atlas.ru>
2023-06-25 12:54:41 +0300

    HW08: Updated main.go

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatalln("It should be more than 2 arguments.")
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env))
 }
